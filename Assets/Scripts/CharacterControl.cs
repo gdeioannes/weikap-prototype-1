8d02f0827d3d943db78fdb3893956e7f22146973
gdeioannes <Trendingames@MacBook-Pro.home>
2016-10-24 00:40:10 -0300

    adding assets and place holder controller

---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControl : MonoBehaviour {
+
+	public GameObject characterSprite;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey(KeyCode.RightArrow)){
+			characterSprite.GetComponent<Rigidbody2D>().AddForce(new Vector2(5,0));
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			characterSprite.GetComponent<Rigidbody2D>().AddForce(new Vector2(-5,0));
+		}
+
+		if(Input.GetKey(KeyCode.UpArrow)){
+			characterSprite.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,5));
+		}
+	}
+}
